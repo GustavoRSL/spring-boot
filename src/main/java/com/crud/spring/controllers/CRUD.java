c0566baf46c6f5299bba62d4ed68ee6c218c13f8
GustavoRSL <d7gustavo@yahoo.com.br>
2022-12-29 11:52:32 -0300

    feat: Adicionando rota deletar usuário

--- src/main/java/com/crud/spring/controllers/CRUD.java
+++ src/main/java/com/crud/spring/controllers/CRUD.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +24,26 @@ public class CRUD {
   private UsuarioRepository usuarioRepository;
 
   //Método GET - Retornar lista de usuários
-  @GetMapping(value = "/Usuarios")
+  @GetMapping(value = "/usuarios")
   @ResponseBody // Retorna os resultados para o corpo da resposta.
   public ResponseEntity<List<Usuario>> listarUsuarios(){
     List<Usuario> usuarios = usuarioRepository.findAll();
     return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); // Retorna a lista em JSON.
-  } 
+  }
+  
+  //Método POST - Cadastra usuário
+  @PostMapping(value = "/cadastro")
+  @ResponseBody
+  public ResponseEntity<Usuario> registrarUsuario(@RequestBody Usuario usuario){
+    Usuario user = usuarioRepository.save(usuario);
+    return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+  }
+
+  //Método DELETE - Deletar usuário
+  @DeleteMapping(value = "/deletar")
+  @ResponseBody
+  public ResponseEntity<String> deletarUsuario(@RequestParam Long idUser){
+    usuarioRepository.deleteById(idUser);
+    return new ResponseEntity<String>("Usuário deletado com sucesso!", HttpStatus.OK);
+  }
 }
